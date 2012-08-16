584d1ce5bfe8d2c8955b15d022d38e65b6e804b1
Pete Fritchman <petef@databits.net>
2012-08-16 11:08:30 -0400

    cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,13 +3,7 @@ require "rspec"
 require "statsdserver/stats"
 
 RSpec.configure do |c|
-  c.before(:all) do
-  end
-
   c.before(:each) do
     @stats = StatsdServer::Stats.new
-  end
-
-  c.after(:each) do
   end
 end
